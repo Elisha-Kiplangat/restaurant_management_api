866d31c03aa85e92298c2ef93e86ab06642e686c
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-09 13:26:10 +0300

    Menu Item Services

--- src/menuItem/menuItem.service.ts
+++ src/menuItem/menuItem.service.ts
@@ -1,6 +1,6 @@
 import db from "../drizzle/db"
-import { MenuItemselect } from "../drizzle/schema"
-
+import { menu_itemTable, MenuItemselect, MenuItemInsert } from "../drizzle/schema"
+import { eq } from "drizzle-orm";
 
 export const menuItemService = async (): Promise<MenuItemselect[]> => {
     try {
@@ -11,4 +11,33 @@ export const menuItemService = async (): Promise<MenuItemselect[]> => {
         console.error('Error fetching menu items:', error);
         throw error;
     }
+}
+
+export const oneMenuItemService = async (id: number): Promise<MenuItemselect | undefined> => {
+    return await db.query.menu_itemTable.findFirst({
+        where: eq(menu_itemTable.id, id)
+    })
+}
+
+export const addMenuItemService = async (menuItem: MenuItemInsert) => {
+    await db.insert(menu_itemTable).values(menuItem)
+    return "menuItem added successfully";
+}
+
+export const updateMenuItemService = async (id: number, menuItem: MenuItemInsert) => {
+    try {
+        const searchedMenuItem = await oneMenuItemService(id);
+        if (!searchedMenuItem) {
+            return false;
+        }
+        await db.update(menu_itemTable).set(menuItem).where(eq(menu_itemTable.id, id));
+        return "menuItem updated successfully";
+    } catch (error) {
+        throw new Error("Failed to update menuItem: ");
+    }
+}
+
+export const deleteMenuItemService = async (id: number) => {
+    await db.delete(menu_itemTable).where(eq(menu_itemTable.id, id));
+    return "menuItem deleted successfully"
 }
