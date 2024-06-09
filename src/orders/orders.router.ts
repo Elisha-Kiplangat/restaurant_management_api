c7e372b9710e87035388b0a49d8e1860753f9494
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-09 11:44:28 +0300

    Services

--- src/orders/orders.router.ts
+++ src/orders/orders.router.ts
@@ -1,8 +1,26 @@
 import {Hono} from 'hono'
-import {ordersController} from './orders.controller'
+import { zValidator } from '@hono/zod-validator';
+import {ordersController, oneOrderController, addOrderController, updateOrderController, deleteOrderController} from './orders.controller'
+import {orderSchema} from '../validator'
 
 export const ordersRouter = new Hono();
 
 ordersRouter.get('orders', ordersController);
+
+//one order
+ordersRouter.get("/orders/:id", oneOrderController)
+
+//add order
+
+ordersRouter.post("orders", zValidator('json', orderSchema, (result, c) => {
+    if (!result.success) {
+        return c.json(result.error, 400)
+    }
+}), addOrderController)
+
+//update a order
+ordersRouter.put("/orders/:id", updateOrderController)
+
+ordersRouter.delete("/orders/:id", deleteOrderController)
 
 export default ordersRouter;
